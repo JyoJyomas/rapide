3df80449fcd6342f51ca276b38c4f0b5182196fb
Aleck Marcial <aleckmarcial@gmail.com>
2024-09-23 22:26:50 +0800

    (v1)navbar

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/67eed877b8.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img class="logo" src="photos/logo.png" alt="rapide-logo">
+        </div>
+        <nav class="navbar">
+            <ul class="navlinks">
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Redeem</a></li>
+                <li><a href="#">Membership</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+            <a href="#" class="user-icon">
+                <i class="fa-regular fa-circle-user fa-2x"></i>
+            </a>
+        </nav>
+    </header> 
+</body>
+</html>
+
