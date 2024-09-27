98c4ab92288f91f92ee7355fd5c7ac7d5a7be1ad
Aleck Marcial <aleckmarcial@gmail.com>
2024-09-27 21:52:58 +0800

    (v1)footer)

---
+++ footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <script src="https://kit.fontawesome.com/67eed877b8.js" crossorigin="anonymous"></script>
+</head>
+    <body>
+        <footer>
+            <div class="footer-content">
+                <img src="photos/logo.png" alt="rapide-logo">
+                <p>Any Concerns? Reach us at any of our platform.</p>
+                <div class="contact">
+                    <div class="socials">       
+                        <a href="https://www.facebook.com/RapideAutoServicePH" target="_blank"><img src="photos/facebook.png" alt="facebook">Rapidé</a>
+                        <a href="https://www.instagram.com/rapideautoph" target="_blank"><img src="photos/instagram.png" alt="instagram">Rapidé</a>
+                    </div>
+                    <div class="sms">
+                        <a><img src="photos/gmail.png" alt="Gmail">info@rapide.com.ph</a>
+                        <a><img src="photos/phone.png" alt="Contacts">0966 061 9979(Globe)<br>/0919 269 4103(Smart)</a>
+                    </div>
+                </div>
+                <div class="loc">
+                    <h2>Main Office:</h2>
+                    <p>Monday to Sunday
+                    <br>8:30 A.M - 5:30 P.M</br>
+                    1166 Chino Roces Avenue, Corner Estrella Avenue, Makati</br></p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
