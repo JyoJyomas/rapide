92983dfb8f7540723721cc1838f360486ac6eb00
Kyla <justkylaestives@gmail.com>
2024-09-29 12:28:44 +0800

    acknowledgement.php

---
+++ Acknowledgement.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="acknowledgement.css">
+    <link href="https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/67eed877b8.js" crossorigin="anonymous"></script>
+    <title>Acknowledgement | Rapidé </title>
+</head>
+<body>
+    <?php 
+        include 'navbar.php'
+    ?>
+        <section class="memcon">     
+            <img class="logo-container" src="rapide logo.png" alt="Logo">
+            <div class="centered-box">
+                <p><strong>Thank you for choosing Rapidé. We’re committed to driving your satisfaction, and we look forward to being a part of your automotive journey.</strong></p>
+            </div>
+            <div class="hose">
+                    <a href = "homepage.php" target="_self"><button class="home-btn">Home</button></a>
+                    <a href = "services2.php" target="_self"><button class="services-btn">Services</button></a>
+            </div>
+        </Section>
+
+    <?php 
+        include 'footer.php'
+   ?>
+</body>
+</html>
+
+
