f8edaf6ad6da2b722acccd18d1d9d4a9d4c6aae6
Kyla <justkylaestives@gmail.com>
2024-09-29 13:07:27 +0800

    login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv= "X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="widthdevice-width, inital-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login | Rapidé </title>
+</head>
+<body>
+<aside class="sidebar">
+        <img src="logo.png" alt="Logo">
+</aside>
+    <div class="login-box">
+            <header>WELCOME</header>
+            <input type="text" class="input-field" placeholder="Email" autocomplete="off" required>        
+            <input type="password" class="input-field" placeholder="Password" autocomplete="off" required>
+            <button class="submit-btn">Login</button>
+            <p class="membership-link"><strong>Don't have an account?</strong> <a href="membership2.php" target="_self">Apply for<br>Membersip</br></a></p>
+        </div>
+    </div>                
+</body>
+<?php
+
+?>
+</html>
