7ef27c7199b208a20629375e4cf7fc92cf749f28
Kai <justkylaestives@gmail.com>
2024-10-04 22:43:43 +0800

    Update servicesGoods.php

--- servicesGoods.php
+++ servicesGoods.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="services(goods).css">
+    <link rel="stylesheet" href="servicesGoods.css">
     <title>Booking Form</title>
     <script src="https://kit.fontawesome.com/67eed877b8.js" crossorigin="anonymous"></script>
 </head>
