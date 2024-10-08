4ac00fd3ea97cce078ff1e859db7128c35516df9
Kyla <justkylaestives@gmail.com>
2024-10-08 17:09:51 +0800

    acknowledgement.php

---
+++ acknowledgement-membership.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="acknowledgement.css">
+    <link href="https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/67eed877b8.js" crossorigin="anonymous"></script>
+    <title>Acknowledgement | Rapidé </title>
+</head>
+<body style="background-color: #FABC3F">
+    <?php 
+        include 'navbar.php'
+    ?>   
+            <img class="logo-container" src="photos/logo2.png" alt="Logo">
+            <div class="centered-box">
+                <p><strong>Thank You for Applying Membership to Rapide! Please wait for 24 hours to validate your membership, thank you.</strong></p>
+            </div>
+            <div class="hose">
+                    <a href = "homepage.php" target="_self"><button class="home-btn">Home</button></a>
+            </div>
+    <?php 
+        include 'footer.php'
+   ?>
+</body>
+</html>
